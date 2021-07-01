f3826d380aa6adfd62385f4a574a767a4b23a663
dev-ted <devted19@gmail.com>
2021-07-01 08:59:29 -0700

    function that adds 2 intergers

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,38 +1,53 @@
-
+#include "holberton.h"
 /**
- * infinite_add - adds integers
- * @n1: interger to be added;
- * @n2: interger to be added;
- * @r: returned;
- * @size_r: size of buffer;
- * Return: r;
+ * infinite_add -  adds two numbers
+ * @n1: first number
+ * @n2: second number
+ * @r: result
+ * @size_r: result lenght
+ * Return: sum
+ *
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
+
 {
-	int l1 = _strlen(n1), l2 = _strlen(n2), bn = l1 > l2 ? l1 : l2;
+	/* local variable declaration */
+	int i = 0, j = 0, k, l = 0, f, s, d = 0;
 
-	int carry = 0, equation = 0;
-
-	size_r--, l1--, l2--;
-
-	if (bn + 1 > size_r)
+	while (n1[i] != '\0')
+		i++;
+	while (n2[j] != '\0')
+		j++;
+	if (i > j)
+		l = i;
+	else
+		l = j;
+	if (l + 1 > size_r)
 		return (0);
-	r[bn + 1] = '\0';
-	for (; bn >= 0 && size_r >= 0; size_r--, bn--, l1--, l2--)
+	r[l] = '\0';
+	for (k = l - 1 ; k >= 0 ; k--)
 	{
-		equation = carry;
-		if (carry)
-			carry = 0;
-		if (l1 >= 0)
-			equation += n1[l1] - '0';
-		if (l2 >= 0)
-			equation += n2[l2] - '0';
-		if (equation > 9)
-		{
-			equation %= 10;
-			carry = 1;
-		}
-		r[bn] = (equation + '0');
+		i--;
+		j--;
+		if (i >= 0)
+			f = n1[i] - '0';
+		else
+			f = 0;
+		if (j >= 0)
+			s = n2[j] - '0';
+		else
+			s = 0;
+		r[k] = (f + s + d) % 10 + '0';
+		d = (f + s + d) / 10;
+	}
+	if (d == 1)
+	{
+		r[l + 1] = '\0';
+		if (l + 2 > size_r)
+			return (0);
+		while (l-- >= 0)
+			r[l + 1] = r[l];
+		r[0] = d + '0';
 	}
 	return (r);
 }
