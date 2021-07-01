2ddc9d564fde0e2a67a4ab41a32e9200419cdc7d
kisala kapenda <18012621@rcconnect.co.za>
2021-07-01 17:15:55 +0200

    a function that adds two numbers.

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,26 @@
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+        int l1 = _strlen(n1), l2 = _strlen(n2), bn = l1 > l2 ? l1 : l2;
+        int carry = 0, equation = 0;
+        size_r--, l1--, l2--;
+        if (bn + 1 > size_r)
+                return (0);
+        r[bn + 1] = '\0';
+        for (; bn >= 0 && size_r >= 0; size_r--, bn--, l1--, l2--)
+        {
+                equation = carry;
+                if (carry)
+                        carry = 0;
+                if (l1 >= 0)
+                        equation += n1[l1] - '0';
+                if (l2 >= 0)
+                        equation += n2[l2] - '0';
+                if (equation > 9)
+                {
+                        equation %= 10;
+                        carry = 1;
+                }
+                r[bn] = (equation + '0');
+        }
+        return (r);
+}
