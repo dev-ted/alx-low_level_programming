625872debfd8b2c4a1fc5277d7cea6685511a822
Teddy <68188559+dev-ted@users.noreply.github.com>
2021-07-07 19:49:10 +0200

    Update 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -17,10 +17,11 @@ int wildcmp(char *s1, char *s2)
 	if (*s1 == '\0' && *s2 == '*' && !*move_char(s2))
 		return (1);
 	if (*s1 == *s2)
+	{
 		if (*s1 == '\0')
 			return (1);
 	return (wildcmp(s1 + 1, s2 + 1));
-
+	}
 	if (*s1 == '\0' || *s2 == '\0')
 		return (0);
 	if (*s2 == '*')
