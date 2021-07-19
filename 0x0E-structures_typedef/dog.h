3277160ac3692067d8b17da9b6e14c8ce1e85e46
dev-ted <devted19@gmail.com>
2021-07-19 03:25:50 -0700

    created go struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - gives dog's info
+ * @name: name of the dog?
+ * @age: age of the dog?
+ * @owner: who owns the dog?
+ *
+ * Description: descripbes the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
